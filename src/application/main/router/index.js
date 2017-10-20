223e5cef3c9a20814c8de75e6b193442aa672d9a
rickyshin <rickychenrihua@foxmail.com>
2017-10-20 17:15:31 +0800

    fix some error

--- src/application/main/router/index.js
+++ src/application/main/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import UiGallery from '@/application/base/gallery'
 
 Vue.use(Router)
 
@@ -11,11 +10,6 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: HelloWorld
-    },
-    {
-      path: '/uiGallery',
-      name: 'UiGallery',
-      component: UiGallery
     }
   ]
 })
