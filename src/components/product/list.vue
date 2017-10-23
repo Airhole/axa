63c2d7d413deb8cc61a947dfb859ad0016437759
rou <rou@Tiny.local>
2017-10-23 17:48:52 +0800

    update products

--- src/components/product/list.vue
+++ src/components/product/list.vue
@@ -14,10 +14,28 @@
     </header>
     <ul class="list-product">
       <li>
+        <img src="placeholder.it/240x240">
         <h2>保险公司产品名称XXX</h2>
         <h3>XXX保险公司</h3>
         <p>特色描述XX XX xx xx xxx xxx</p>
+        <div class="bar">
+          <a href>203.00 起</a>
+        </div>
+      </li>
+      <li>
         <img src="placeholder.it/240x240">
+        <h2>保险公司产品名称XXX</h2>
+        <h3>XXX保险公司</h3>
+        <p>特色描述XX XX xx xx xxx xxx</p>
+        <div class="bar">
+          <a href>203.00 起</a>
+        </div>
+      </li>
+      <li>
+        <img src="placeholder.it/240x240">
+        <h2>保险公司产品名称XXX</h2>
+        <h3>XXX保险公司</h3>
+        <p>特色描述XX XX xx xx xxx xxx</p>
         <div class="bar">
           <a href>203.00 起</a>
         </div>
@@ -44,11 +62,59 @@
 </script>
 <style lang="scss">
   .list-product{
-    border-top: 1px solid #ccc;
-    border-bottom: 1px solid #ccc;
-    + .list-product {
+    list-style: none;
+    > li {
+      border-top: 1px solid #ccc;
+      border-bottom: 1px solid #ccc;
+      padding: rem-calc(10);
+      overflow: hidden;
+      line-height: rem-calc(30);
+    }
+    > li + li {
       margin-top: -1px;
     }
+    h2, h3{
+      font-weight: normal;
+      font-size:rem-calc(18);
+    }
+    h3{
+      color: #f54000;
+    }
+    img{
+      display: block;
+      width: rem-calc(120);
+      height: rem-calc(120);
+      float:left;
+      margin-right: rem-calc(12);
+    }
+    .bar{
+      text-align: right;
+      a {
+        text-decoration: none;
+      }
+    }
   }
-
+</style>
+<style lang="scss" scoped>
+  header{
+    height: rem-calc(44);
+    display: flex;
+    background-color: #dedede;
+    select{
+      flex: 1;
+      height: rem-calc(44);
+      border: 0 none;
+      line-height: rem-calc(44);
+      border-radius: 0;
+      background-color: white;
+      padding-left: rem-calc(8);
+    }
+    input {
+      margin: rem-calc(5) rem-calc(8);
+      height: rem-calc(34);
+      line-height: rem-calc(24);
+      padding: rem-calc(5) rem-calc(8);
+      flex: 2;
+    }
+  }
 </style>
