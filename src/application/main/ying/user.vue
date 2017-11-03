8bfd0df5b46449a34a8b9a0fa9ce18d457866472
ying <ying@yingdeMacBook-Pro.local>
2017-11-03 16:12:37 +0800

    我的页面布局完成

--- src/application/main/ying/user.vue
+++ src/application/main/ying/user.vue
@@ -2,12 +2,12 @@
   <div class="user">
     <div class="user-bg">
       <p>近七天數據</p>
-      <p>37280.00</p>
+      <p>{{ number }}</p>
       <p>微店成交額(元）</p>
     </div>
     <div class="user-list">
-      <div class="user-list-item">訂單查詢</div>
-      <div class="user-list-item">我的積分</div>
+      <router-link to="order_inquiry" class="user-list-item">訂單查詢</router-link>
+      <router-link to="my_points" class="user-list-item">我的積分</router-link>
     </div>
   </div>
 </template>
@@ -17,6 +17,7 @@
     name: 'user',
     data () {
       return {
+        number: '37280.00'
       }
     },
     methods: {
@@ -31,20 +32,55 @@
       background-size: 100%;
       background-repeat: no-repeat;
       width: 100%;
+      color: #fff;
+      text-align: center;
+      p:nth-child(1) {
+        font-size: rem-calc(15);
+        padding-top: rem-calc(35)
+      }
+      p:nth-child(2) {
+        font-size: rem-calc(48);
+        padding: rem-calc(15) 0;
+      }
+      p:nth-child(3) {
+        font-size: rem-calc(12);
+        padding-bottom: rem-calc(50)
+      }
     }
-    color: #fff;
-    text-align: center;
-    p:nth-child(1) {
-      font-size: rem-calc(15);
-      padding-top: rem-calc(35)
-    }
-    p:nth-child(2) {
-      font-size: rem-calc(48);
-      padding: rem-calc(15) 0;
-    }
-    p:nth-child(3) {
-      font-size: rem-calc(12);
-      padding-bottom: rem-calc(50)
+    .user-list {
+      background-color: #f7f7f7;
+      position: relative;
+      text-align: center;
+      display: flex;
+      border-bottom: .5px solid #e5e5e5;
+      .user-list-item {
+        flex: 1;
+        display: block;
+        color: #333;
+        text-decoration: none;
+        font-size: rem-calc(15);
+        cursor: pointer;
+        padding: rem-calc(15);
+      }
+      .user-list-item:first-child {
+        border-right: .5px solid #e5e5e5;
+      }
+      .user-list-item:before {
+        content: "";
+        display: inline-block;
+        vertical-align: middle;
+        background-repeat: no-repeat;
+        background-size: 100%;
+        width: rem-calc(43);
+        height: rem-calc(43);
+        margin-right: rem-calc(5);
+      }
+      .user-list-item:first-child:before {
+        background-image: url("~@/assets/image/user/user_icon1.png");
+      }
+      .user-list-item:last-child:before {
+        background-image: url("~@/assets/image/user/user_icon2.png");
+      }
     }
   }
 </style>
