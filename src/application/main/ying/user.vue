76c3db00c8f83c2126fddebc575e3e8a0e5e17a3
ying <ying@yingdeMacBook-Pro.local>
2017-11-03 15:44:15 +0800

    user

--- src/application/main/ying/user.vue
+++ src/application/main/ying/user.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="user">
-    <div class="content">user</div>
+    <div class="user-bg">
+      <p>近七天數據</p>
+      <p>37280.00</p>
+      <p>微店成交額(元）</p>
+    </div>
+    <div class="user-list">
+      <div class="user-list-item">訂單查詢</div>
+      <div class="user-list-item">我的積分</div>
+    </div>
   </div>
 </template>
 
@@ -18,6 +26,25 @@
 
 <style lang="scss" scoped>
   .user {
-
+    .user-bg {
+      background-image: url("~@/assets/image/user/user_bg.png");
+      background-size: 100%;
+      background-repeat: no-repeat;
+      width: 100%;
+    }
+    color: #fff;
+    text-align: center;
+    p:nth-child(1) {
+      font-size: rem-calc(15);
+      padding-top: rem-calc(35)
+    }
+    p:nth-child(2) {
+      font-size: rem-calc(48);
+      padding: rem-calc(15) 0;
+    }
+    p:nth-child(3) {
+      font-size: rem-calc(12);
+      padding-bottom: rem-calc(50)
+    }
   }
 </style>
