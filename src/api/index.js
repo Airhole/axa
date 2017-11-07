5435b49f3afe3663d1a5b176055408611e694894
Administractor <admin@example.com>
2017-11-07 14:20:47 +0800

    add insure-success

--- src/api/index.js
+++ src/api/index.js
@@ -50,6 +50,9 @@ export const IDEMO = path(demo, true)
 export const IAPP_INIT = path('/init', true)
 // 公司介绍
 export const ICOMPANY_INTRODUCTION = path('/company', true)
+// 投保结果
+let insure = '/result'
+export const INSURE_RESULT = path(insure, true)
 
 export const ENROLL_INTERSET = path(demo)
 export const QUERY_DICT = path(demo)
